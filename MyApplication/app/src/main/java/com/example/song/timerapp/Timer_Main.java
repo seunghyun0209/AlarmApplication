654874c30cf681f94441f2dc01a2f2bdaa096a66
Song <sunghyun020@naver.com>
2017-03-09 10:48:37 +0900

    기본 베이스

---
+++ MyApplication/app/src/main/java/com/example/song/timerapp/Timer_Main.java
@@ -0,0 +1,13 @@
+package com.example.song.timerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Timer_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer__main);
+    }
+}
